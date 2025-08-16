73359cce2e566733cb05a324a91ece26f022029b
Neverland <578463782@qq.com>
2025-08-17 07:47:29 +0800

    Fix: Correct fireball position calculation error in Q skill

---
+++ Assets/Demo/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace XSkillSystem
+{
+    public class EnemyController : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<TeamProvider>().TeamId = 2;
+        }
+    }
+}
